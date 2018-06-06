4bcc7d57b5508f86feff3a8bfab3b0daa2dcc169
Hisato Kawaminami and Swati Sahay <swati@>
2018-06-06 09:06:49 -0700

    added js script for leapyear

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(function(){
+
+  $("#submit").click(function(){
+    var year= $("input#leap").val();
+    var output=leapyear(year)
+    $(".year").text(year)
+    if(!output){
+      $(".not").text("not");
+    }
+    else{
+      $(".not").text("");
+
+    }
+    $("#output").show();
+
+  });
+});
+
+// business Logic
+var leapyear=function(year){
+  if((year%4===0)&&(year%100!==0)||(year%400===0)){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
